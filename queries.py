d0b0f8b0be52b15711a6e39ca8840a158fe3f3b8
Sz. N. Bálint <szalaynb93@gmail.com>
2017-09-10 18:48:03 +0200

    SQL system is ready for use

--- queries.py
+++ queries.py
@@ -4,33 +4,34 @@ import database_common
 # if __name__ == __main__:
 
 
-show_mentors = """SELECT mentors.first_name, mentors.last_name,
-                                schools.name, schools.country FROM mentors
+show_mentors = """SELECT mentors.first_name AS mentor_first_name, mentors.last_name AS mentor_last_name,
+                                schools.name AS school_name, schools.country AS country FROM mentors
                                 JOIN schools ON mentors.city = schools.city
                                 ORDER BY mentors.id ASC"""
 
-show_all_schools = """SELECT mentors.first_name , mentors.last_name,
-                                schools.name, schools.country FROM mentors
+show_all_schools = """SELECT mentors.first_name AS mentor_first_name, mentors.last_name AS mentor_last_name,
+                                schools.name AS school_name, schools.country AS country FROM mentors
                                 FULL JOIN schools ON mentors.city = schools.city
                                 ORDER BY mentors.id ASC"""
 
-show_mentors_by_country = """SELECT schools.country, COUNT(mentors.id) AS count FROM mentors
+show_mentors_by_country = """SELECT schools.country AS country, COUNT(mentors.id) AS count FROM mentors
                             JOIN schools ON mentors.city = schools.city
                             GROUP BY schools.country
                             ORDER BY schools.country ASC"""
 
-show_contacts = """SELECT DISTINCT schools.name, mentors.first_name, mentors.last_name
-                    FROM schools LEFT JOIN mentors ON schools.contact_person = mentors.id
+show_contacts = """SELECT DISTINCT schools.name AS school, mentors.first_name AS first_name,
+                    mentors.last_name AS last_name FROM schools
+                    LEFT JOIN mentors ON schools.contact_person = mentors.id
                     ORDER BY schools.name"""
 
-show_applicants = """SELECT applicants.first_name, applicants.application_code,
-                                applicants_mentors.creation_date FROM applicants
+show_applicants = """SELECT applicants.first_name AS a_first_name, applicants.application_code AS application_code,
+                                applicants_mentors.creation_date AS creation_date FROM applicants
                                 JOIN applicants_mentors ON applicants.id = applicants_mentors.applicant_id
                                 WHERE applicants_mentors.creation_date >= '2016-01-01'
                                 ORDER BY applicants_mentors.creation_date DESC"""
 
 show_applicants_and_mentors = """SELECT applicants.first_name AS applicant_first_name,
-                            applicants.application_code,
+                            applicants.application_code AS application_code,
                             mentors.first_name AS mentor_first_name,
                             mentors.last_name AS mentor_last_name
                             FROM applicants_mentors
--- server.py
+++ server.py
@@ -13,49 +13,49 @@ def route_list():
 @app.route("/mentors")
 def mentors_list():
     titles = ["First Name", "Last Name", "School Name", "Country"]
-    #titles = ["mentors.first_name", "mentors.last_name", "schools.name", "schools.country"]
+    actual_keys = ["mentor_first_name", "mentor_last_name", "school_name", "country"]
     to_the_table = queries.mass_executor(queries.show_mentors)
-    return render_template("result.html", to_the_table=to_the_table, titles=titles)
+    return render_template("result.html", to_the_table=to_the_table, titles=titles, actual_keys=actual_keys)
 
 
 @app.route("/all-schools")
 def schools_list():
     titles = ["First Name", "Last Name", "School Name", "Country"]
-    #titles = [mentors.first_name , mentors.last_name,schools.name, schools.country]
+    actual_keys = ["mentor_first_name", "mentor_last_name", "school_name", "country"]
     to_the_table = queries.mass_executor(queries.show_all_schools)
-    return render_template("result.html", to_the_table=to_the_table, titles=titles)
+    return render_template("result.html", to_the_table=to_the_table, titles=titles, actual_keys=actual_keys)
 
 
 @app.route("/mentors-by-country")
 def countries_list():
     titles = ["Country", "Number of Mentors"]
-    #titles = [schools.country, count]
+    actual_keys = ["country", "count"]
     to_the_table = queries.mass_executor(queries.show_mentors_by_country)
-    return render_template("result.html", to_the_table=to_the_table, titles=titles)
+    return render_template("result.html", to_the_table=to_the_table, titles=titles, actual_keys=actual_keys)
 
 
 @app.route("/contacts")
 def contacts_list():
     titles = ["School", "First Name", "Last Name"]    
-    #schools.name, mentors.first_name, mentors.last_name
+    actual_keys = ["school", "first_name", "last_name"]
     to_the_table = queries.mass_executor(queries.show_contacts)
-    return render_template("result.html", to_the_table=to_the_table, titles=titles)
+    return render_template("result.html", to_the_table=to_the_table, titles=titles, actual_keys=actual_keys)
 
 
 @app.route("/applicants")
 def applicants_list():
     titles = ["First Name", "Application Code", "Creation Date"]
-    #applicants.first_name, applicants.application_code, applicants_mentors.creation_date
+    actual_keys = ["a_first_name", "application_code", "creation_date"]
     to_the_table = queries.mass_executor(queries.show_applicants)
-    return render_template("result.html", to_the_table=to_the_table, titles=titles)
+    return render_template("result.html", to_the_table=to_the_table, titles=titles, actual_keys=actual_keys)
 
 
 @app.route("/applicants-and-mentors")
 def connections_list():
     titles = ["Applicant F. Name", "Apllication Code", "Mentor F. Name", "Mentor L. Name"]
-    #applicant_first_name, applicants.application_code, mentor_first_name, mentor_last_name
+    actual_keys = ["applicant_first_name", "application_code", "mentor_first_name", "mentor_last_name"]
     to_the_table = queries.mass_executor(queries.show_applicants_and_mentors)
-    return render_template("result.html", to_the_table=to_the_table, titles=titles)
+    return render_template("result.html", to_the_table=to_the_table, titles=titles, actual_keys=actual_keys)
 
 
 if __name__ == "__main__":
